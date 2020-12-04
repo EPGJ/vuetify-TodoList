cd6b2e468bf0b672f08990c0c2b73dbfa3add49b
Elias Pereira Gomes Junior <eliasepgj@gmail.com>
2020-12-04 14:56:43 -0300

    Vuetify todo list complete

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about pa-6" >
+    <h1>About Vuetify todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil
+      voluptates sit amet quod! Voluptatum iure suscipit, mollitia et ex nam
+      quos doloremque, veniam ipsa deserunt blanditiis eos sunt autem.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil
+      voluptates sit amet quod! Voluptatum iure suscipit, mollitia et ex nam
+      quos doloremque, veniam ipsa deserunt blanditiis eos sunt autem.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nihil
+      voluptates sit amet quod! Voluptatum iure suscipit, mollitia et ex nam
+      quos doloremque, veniam ipsa deserunt blanditiis eos sunt autem.
+    </p>
+  </div>
+</template>
